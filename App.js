dafbff2759a2850d23d319f89806d61dcc21e2d8
te-kaveridaddimani <kaveri.d@testyantra.in>
2021-05-22 12:13:13 +0530

    employee management system

---
+++ App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { empsystem } from "./employemanagementesystem/empsystem.js";
+
+function App() {
+  return <BrowserRouter>
+                
+  {empsystem}
+  
+</BrowserRouter>;
+}
+
+export default App;
+
